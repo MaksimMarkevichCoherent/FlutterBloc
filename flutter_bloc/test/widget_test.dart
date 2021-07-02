030b3ece2fde4ba585b6811d9f05cc61c6750f9f
Maksim Markevich <MaksimMarkevich@coherentsolutions.com>
2021-07-02 14:21:37 +0300

    * implemented login bloc

--- flutter_bloc/test/widget_test.dart
+++ flutter_bloc/test/widget_test.dart
@@ -6,14 +6,19 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:user_repository/user_repository.dart';
 import 'package:flutter_bloc_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App(
+      authenticationRepository: AuthenticationRepository(),
+      userRepository: UserRepository(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
